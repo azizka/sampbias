562739ee52e674b76849683c1ec923976afa2374
Alexander Zizka <az64mycy@rstudio1.idiv.de>
2020-07-24 11:04:42 +0200

    fix bug for rarefaction and simulation

--- empirical_analyses_simulation.R
+++ empirical_analyses_simulation.R
@@ -6,57 +6,58 @@ library(sp)
 library(rgdal)
 library(sf)
 library(raster)
-
+library(readr)
 
 # rarefaction steps:
-rar <- c(1, 0.5, 0.25, 0.1, 0.01, 0.001, 0.0001)
-ID <- 1
+rar <- c(1, 0.5, 0.25, 0.1, 0.01, 0.001)
+rar <- rar[1]
+ID <- 1:5
 res <- 0.05
 
-#rarefaction of a randomly sampled dataset across the study area without bias
+#rarefaction of a randomly sampled data set across the study area without bias
 
-## get a polygon of borneo
+## get a polygon of Borneo
 data(landmass)
 
 born2 <- st_read("empirical_analyses/Borneo.kml")
 born2 <- sf:::st_zm(born2$geom)
 born2 <- as(born2, 'Spatial')
-born <- intersect(born, born2)
+born <- intersect(landmass, born2)
 
 
-## randomly sample points across teh island
+## randomly sample points across the island
 occ <- sp::spsample(x = born, n = 100000, type = "random")
 occ <- data.frame(species = letters[1:25],
                   coordinates(occ))
 names(occ) = c("species", "decimalLongitude", "decimalLatitude")
 
 # rarefaction loop for the bias calculation, write out the resulting paramter estimates and a map
-for(i in 1:length(rar)){
+for(i in 1:length(ID)){
   print(i)
   # subset the simulated data
-  sub <- occ[base::sample(x = 1:nrow(occ), size = round(nrow(occ) * rar[i], 0)), ]
+  sub <- occ[base::sample(x = 1:nrow(occ), size = round(nrow(occ) * rar, 0)), ]
 
   # calculate sampling bias
-  out <- calculate_bias(occ, res = res, buffer = 0.5)
+  out <- calculate_bias(sub, res = res, buffer = 0.5)
 
   # plot of bias projection in space
   proj <- project_bias(out)
   p2 <- map_bias(proj)
 
   ggsave(p2, filename = paste("empirical_analyses/simulations/figure_empirical_results_spatial_projection_simulated_",
-                              ID, "_", rar[i], ".pdf", sep = ""),
+                              ID[i], "_", rar, ".pdf", sep = ""),
          height = 16, width = 16)
 
 
-  #preapre output files
+  #prepare output files
   out <- out$bias_estimate
-  out$ID <- ID
-  out$rar <- rar[i]
+  out$ID <- ID[i]
+  out$rar <- rar
   out$res <- res
   out$type <- "simulated"
 
   # write to disk
-  write_csv(out, "empirical_analyses/weight_estimates.csv", append = TRUE)
+  write_csv(out, "empirical_analyses/simulations/weight_estimates.csv", append = TRUE)
 }
 
 
